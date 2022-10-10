c6c9eff0ce2cf510fcb804ba355cc2870d9ba7e9
ZalfahChordelia <2225210092@untirta.ac.id>
2022-10-10 13:04:25 +0700

    tugasdb

--- untirta/settings.py
+++ untirta/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'untirta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'untirta',
-        'USER': 'root',
-        'PASSWORD': 'Real?pcy27',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
